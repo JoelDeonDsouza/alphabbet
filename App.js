3d1f9159910b7cd0c8d7a9bb16a600f64fa15ab1
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-17 15:27:41 +0100

    UI of AlphaBBET finished

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
 import { colors } from "./src/constent";
 import Keyboard from "./src/components/Keyboard";
 
+const LIMITEDTRYS = 6;
+
 export default function App() {
+  const word = "joel";
+  const letters = word.split("");
+  const rows = new Array(LIMITEDTRYS).fill(new Array(letters.length).fill(""));
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>AlphaBET</Text>
+      <ScrollView style={styles.map}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell) => (
+              <View style={styles.cell}>
+                <Text style={styles.text}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
       <Keyboard />
     </SafeAreaView>
   );
@@ -25,5 +41,30 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     letterSpacing: 5,
     marginTop: 50,
+  },
+  map: {
+    padding: 20,
+    alignSelf: "stretch",
+    height: 100,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.lightGrey,
+    flex: 1,
+    maxWidth: 70,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: colors.lightGrey,
+    fontWeight: "bold",
+    fontSize: 26,
   },
 });
