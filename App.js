5f3e9554db4685aab41a30f3d42f012c35cbffd3
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-20 21:17:05 +0100

    rendered Clear Cell, Center new Colum, Add to new Cell and filter to limited rows functionalities

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
-import { colors } from "./src/constent";
+import { colors, CLEAR, ENTER } from "./src/constent";
 import Keyboard from "./src/components/Keyboard";
 
 const LIMITEDTRYS = 6;
@@ -20,19 +20,56 @@ export default function App() {
   const [curCol, setCurCol] = useState(0);
 
   const onKeyPressed = (key) => {
+    //Clear cell
     const reNewedRows = copyArray(rows);
-    reNewedRows[curRow][curCol] = key;
-    setRows(reNewedRows);
+    if (key === CLEAR) {
+      const beforeCol = curCol - 1;
+      if (beforeCol >= 0) {
+        reNewedRows[curRow][beforeCol] = "";
+        setRows(reNewedRows);
+        setCurCol(beforeCol);
+      }
+      return;
+    }
+    //Center to new Colum
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+      return;
+    }
+
+    //Add to the cell
+    if (curCol < rows[0].length) {
+      reNewedRows[curRow][curCol] = key;
+      setRows(reNewedRows);
+      setCurCol(curCol + 1);
+    }
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
   };
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>AlphaBET</Text>
       <ScrollView style={styles.map}>
-        {rows.map((row) => (
-          <View style={styles.row}>
-            {row.map((cell) => (
-              <View style={styles.cell}>
+        {rows.map((row, r) => (
+          <View key={`row-${r}`} style={styles.row}>
+            {row.map((cell, j) => (
+              <View
+                key={`cell-${r}-${j}`}
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(r, j)
+                      ? colors.darkGrey
+                      : colors.lightGrey,
+                  },
+                ]}
+              >
                 <Text style={styles.text}>{cell.toUpperCase()}</Text>
               </View>
             ))}
